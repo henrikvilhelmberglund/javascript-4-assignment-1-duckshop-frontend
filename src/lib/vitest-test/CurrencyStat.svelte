9c474901ee5386f706af0ea6a3d4020fd2ee64e2
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-11-02 17:49:11 +0100

    Initial commit

---
+++ src/lib/vitest-test/CurrencyStat.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { formatCurrency } from './formatCurrency';
+
+	export let value: number;
+</script>
+
+<div>
+	<p class="text-3xl p-24">
+    <!-- this should fail -->
+		<!-- {value} -->
+
+    <!-- this should work (only one at a time) -->
+		{formatCurrency(value)}
+	</p>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<header class="flex items-center">
+	<h1 class="text-5xl py-4">Welcome to SvelteKit!</h1>
+	<span class="i-carbon-logo-svelte text-6xl bg-[#f73c00]" />
+</header>
+<h2 class="text-xl">(and my template!)</h2>
+<p>
+	Visit <a class="underline decoration-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+	read the documentation
+</p>
