0b4b6c6e9502296dfb1fae13dcdd6ca65868064d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-06 23:43:38 +0100

    fix: use relative and absolute to avoid logo shifting when scrollbar exists/doesn't exist

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,26 @@
 <script>
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
-<header class="flex items-center justify-center bg-slate-800 py-4">
-	<h1 class="font-agbalumo pb-1 text-5xl text-white">DuckShop</h1>
+<header class="relative top-0 h-20 w-screen items-center justify-center bg-slate-800 py-4">
+	<h1 class="font-agbalumo absolute left-[50%] -translate-x-[50%] pb-1 text-5xl text-white">
+		DuckShop
+	</h1>
 </header>
 
 <slot />
 
-<footer class="mt-24" />
+<footer class="mt-14" />
 
 <style></style>
