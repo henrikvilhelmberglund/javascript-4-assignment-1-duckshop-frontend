a9da684dc381427bf12430b063bd9364a8fe6faf
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-06 22:46:28 +0100

    fix: rename interface IProduct

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import type { Product } from '$lib/interfaces/product.js';
+	import Product from '$lib/components/Product.svelte';
+	import type { IProduct } from '$lib/interfaces/product.js';
 
 	export let data;
-	const products: Product[] = data.products;
+	const products: IProduct[] = data.products;
 </script>
 
 <header class="flex items-center py-4 justify-center bg-slate-800">
@@ -17,18 +18,8 @@
 
 <main class="flex flex-col items-center mt-24">
 	<div class="flex w-[80vw] flex-wrap gap-24">
-		{#each products as { id, name, price, description, amount_in_stock, image_link }}
-			<a href={`/products/${id}`}>
-				<article
-					class="flex relative text-center flex-col h-min w-[300px] rounded-3xl bg-slate-200"
-				>
-					<img class="rounded-3xl rounded-b-none" src={image_link} alt={name} />
-					<h3 class="text-2xl">{name}</h3>
-					<div class="absolute bg-slate-200 border-black border-1 rounded p-4 bottom-8 right-0">
-						<p class="">{price} SEK</p>
-					</div>
-				</article>
-			</a>
+		{#each products as product}
+			<Product {product}/>
 		{/each}
 	</div>
 </main>
