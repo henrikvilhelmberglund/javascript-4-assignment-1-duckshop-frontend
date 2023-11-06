69d119d4407d67ac69bb1645338668e42a0bf229
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-06 23:50:14 +0100

    feat: add todo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,7 @@
 	});
 </script>
 
+<!-- TODO fix ugly horizontal scrollbar  -->
 <header class="relative top-0 h-20 w-screen items-center justify-center bg-slate-800 py-4">
 	<h1 class="font-agbalumo absolute left-[50%] -translate-x-[50%] pb-1 text-5xl text-white">
 		DuckShop
