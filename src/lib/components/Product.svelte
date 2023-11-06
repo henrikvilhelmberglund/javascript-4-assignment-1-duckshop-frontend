bb705d037bc165f6b757f0e382cf2c1a82e39326
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-06 22:46:46 +0100

    feat: implement detail page markup WIP

--- src/lib/components/Product.svelte
+++ src/lib/components/Product.svelte
@@ -1,18 +1,40 @@
 <script lang="ts">
-	import type { Product } from '$lib/interfaces/product';
+	import type { IProduct } from '$lib/interfaces/product';
 
-	export let product: Product;
-  export let detailPage = false;
+	export let product: IProduct;
+	export let detailPage = false;
 	const { id, name, price, description, amount_in_stock, image_link } = product;
 </script>
 
-<article class="flex relative text-center flex-col h-min w-[300px] rounded-3xl bg-slate-200">
-	<img class="rounded-3xl rounded-b-none" src={image_link} alt={name} />
-	<h3 class="text-2xl">{name}</h3>
-	<div class="absolute bg-slate-200 border-black border-1 rounded p-4 bottom-8 right-0">
-		<p class="">{price} SEK</p>
-	</div>
-</article>
+{#if !detailPage}
+	<!-- if not detail page (/product/:id) wrap article with a link -->
+	<a href={`/products/${id}`}>
+		<article class="flex relative text-center flex-col h-min w-[300px] rounded-3xl bg-slate-200">
+			<img class="rounded-3xl rounded-b-none" src={image_link} alt={name} />
+			<h3 class="text-2xl">{name}</h3>
+			<div class="absolute bg-slate-200 border-black border-1 rounded p-4 bottom-8 right-0">
+				<p class="">{price} SEK</p>
+			</div>
+		</article>
+	</a>
+{:else}
+	<main class="flex">
+		<article class="flex relative text-center flex-col h-min w-[512px] rounded-3xl bg-slate-200">
+			<img class="rounded-3xl rounded-b-none" src={image_link} alt={name} />
+			<h3 class="text-2xl">{name}</h3>
+			<div class="absolute bg-slate-200 border-black border-1 rounded p-4 bottom-8 right-0">
+				<p class="">{price} SEK</p>
+			</div>
+		</article>
+		<section class="pl-6 w-96 flex-col justify-between flex">
+			<p class="italic text-xl">
+				{description}
+			</p>
+      <!-- TODO implement cart -->
+			<button class="self-start p-4 bg-green-400 rounded-md mb-8">Add to cart</button>
+		</section>
+	</main>
+{/if}
 
 <style>
 </style>
