16d0e387f25c63913be86245eb0e20a602b57c77
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-06 17:41:29 +0100

    feat: add index route which loops through the products and creates cards for each one

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,36 @@
-<header class="flex items-center">
-	<h1 class="text-5xl py-4">Welcome to SvelteKit!</h1>
-	<span class="i-carbon-logo-svelte text-6xl bg-[#f73c00]" />
+<script lang="ts">
+	import type { Product } from '$lib/interfaces/product.js';
+
+	export let data;
+	const products: Product[] = data.products;
+</script>
+
+<header class="flex items-center py-4 justify-center bg-slate-800">
+	<h1 class="text-5xl pb-1 font-agbalumo text-white">DuckShop</h1>
 </header>
-<h2 class="text-xl">(and my template!)</h2>
-<p>
-	Visit <a class="underline decoration-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to
-	read the documentation
-</p>
+
+<section class="flex justify-center">
+	<h2 class="text-3xl">
+		Welcome to DuckShop, your goto shop for duck related (and non duck related) products!
+	</h2>
+</section>
+
+<main class="flex flex-col items-center mt-24">
+	<div class="flex w-[80vw] flex-wrap gap-24">
+		{#each products as { id, name, price, description, amount_in_stock, image_link }}
+			<a href={`/products/${id}`}>
+				<article
+					class="flex relative text-center flex-col h-min w-[300px] rounded-3xl bg-slate-200"
+				>
+					<img class="rounded-3xl rounded-b-none" src={image_link} alt={name} />
+					<h3 class="text-2xl">{name}</h3>
+					<div class="absolute bg-slate-200 border-black border-1 rounded p-4 bottom-8 right-0">
+						<p class="">{price} SEK</p>
+					</div>
+				</article>
+			</a>
+		{/each}
+	</div>
+</main>
+
+<footer class="mt-24" />
