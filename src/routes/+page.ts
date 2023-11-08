669e9d926b11740d71daa6850883fdbeb1246799
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-08 14:03:28 +0100

    fix: fix interface (2)

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 import type { IProduct } from '$lib/interfaces/product';
 
 /** @type {import('./$types').PageLoad} */
-export async function load(): Promise<{ products: Product[] }> {
+export async function load(): Promise<{ products: IProduct[] }> {
 	const response = await fetch('http://localhost:3001/api/v1/products');
 	const data = await response.json();
 	const products: IProduct[] = data.mockData;
