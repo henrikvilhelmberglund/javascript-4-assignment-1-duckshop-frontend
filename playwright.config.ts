77c629eb02a91bdf9b074dc11c113740aa98095f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-08 20:33:35 +0100

    fix: use !process.env.CI for reuseExistingServer because it is more standard

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,13 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  webServer: {
-    // default config to run build and run preview, but slow so using dev instead 😇
+	webServer: {
+		// default config to run build and run preview, but slow so using dev instead 😇
 		// command: 'npm run build && npm run preview',
 		// port: 4173
 		command: 'npm run dev',
 		port: 5173,
-		reuseExistingServer: true,
+		reuseExistingServer: !process.env.CI,
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
