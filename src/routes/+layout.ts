dcd272cae20e7ce5183284ffafb0b1808d2687da
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-08 21:48:26 +0100

    feat: use FETCH_URL

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,11 +2,13 @@
 export const ssr = false;
 export const prerender = false;
 
+import { FETCH_URL } from '$lib/constants';
 import type { IProduct } from '$lib/interfaces/product';
 
 /** @type {import('./$types').PageLoad} */
 export async function load(): Promise<{ products: IProduct[] }> {
-	const response = await fetch('http://localhost:3001/api/v1/products');
+	// TODO switch back to localhost
+	const response = await fetch(`http://${FETCH_URL}:3001/api/v1/products`);
 	const data = await response.json();
 	const products: IProduct[] = data.mockData;
 	return {
--- src/routes/products/[id]/+page.ts
+++ src/routes/products/[id]/+page.ts
@@ -1,11 +1,12 @@
+import { FETCH_URL } from '$lib/constants.js';
 import type { IProduct } from '$lib/interfaces/product';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }): Promise<{ product: IProduct }> {
-	const response = await fetch(`http://localhost:3001/api/v1/product/${params.id}`);
+	const response = await fetch(`http://${FETCH_URL}:3001/api/v1/product/${params.id}`);
 	const data = await response.json();
 	const product: IProduct = data.mockData;
 	return {
-		product
+		product,
 	};
 }
