190a8fb741bee506a9d35d52e4c2d92917799a02
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-08 14:05:08 +0100

    feat: move products load function to layout so I can access it from every route

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,15 @@
 // disable SSR and prerendering to make sure fetches are done only in the browser
 export const ssr = false;
 export const prerender = false;
+
+import type { IProduct } from '$lib/interfaces/product';
+
+/** @type {import('./$types').PageLoad} */
+export async function load(): Promise<{ products: IProduct[] }> {
+	const response = await fetch('http://localhost:3001/api/v1/products');
+	const data = await response.json();
+	const products: IProduct[] = data.mockData;
+	return {
+		products
+	};
+}
