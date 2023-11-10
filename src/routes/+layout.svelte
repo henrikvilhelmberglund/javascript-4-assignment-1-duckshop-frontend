6e1ec3e6040a5b48e286c50638d9398bd61d0c68
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-10 23:05:13 +0100

    feat: use totalProducts to display a number next to the cart

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onNavigate } from '$app/navigation';
+	import { totalProducts } from '$lib/stores';
 
 	onNavigate((navigation) => {
 		if (!document.startViewTransition) return;
@@ -16,12 +17,19 @@
 <!-- TODO fix ugly horizontal scrollbar  -->
 <header class="relative top-0 h-20 w-screen items-center justify-center bg-slate-800 py-4">
 	<a href="/">
-		<h1 class="font-agbalumo absolute md:left-[50%] md:-translate-x-[50%] pb-2 md:pb-1 pl-3 text-4xl md:text-5xl text-white">
+		<h1
+			class="font-agbalumo absolute pb-2 pl-3 text-4xl text-white md:left-[50%] md:-translate-x-[50%] md:pb-1 md:text-5xl">
 			DuckShop
 		</h1>
 	</a>
 	<div class="flex justify-end gap-12 pr-12 text-xl text-slate-700">
-		<a class="bg-slate-50 p-2 rounded" href="/cart">View Cart</a>
+		<a class="rounded bg-slate-50 p-2" href="/cart">View Cart</a>
+		{#if $totalProducts > 0}
+			<p
+				class="border-1 absolute right-10 top-12 h-6 w-6 rounded-full border-black bg-blue-200 text-center text-sm leading-6">
+				{$totalProducts}
+			</p>
+		{/if}
 	</div>
 </header>
 
