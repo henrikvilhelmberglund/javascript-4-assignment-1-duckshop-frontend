36d560993f8a67c5f658c4cc46d6493ad51bcfa7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-10 21:50:07 +0100

    feat: add DisplayMessage component for displaying a message (like a popup)

---
+++ src/lib/components/DisplayMessage.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { message } from '$lib/store';
+	import { onDestroy, onMount } from 'svelte';
+
+	let timeout: number;
+
+	onMount(() => {
+		timeout = window.setTimeout(() => {
+			$message = '';
+		}, 1000);
+	});
+
+	onDestroy(() => {
+		window.clearTimeout(timeout);
+		$message = '';
+	});
+</script>
+
+<div
+	class="animate-delay-0.5s keyframes-fade-out absolute -left-20 bottom-20 animate-[fade-out_0.6s_ease-out_1] rounded bg-green-400 p-4">
+	<p class="text-xl">
+		{$message}
+	</p>
+</div>
+
+<style>
+</style>
