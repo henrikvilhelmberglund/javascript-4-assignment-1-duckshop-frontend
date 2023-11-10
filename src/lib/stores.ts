317552058739e9c1e0a50b70b84eeb897e4ff873
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-10 23:03:51 +0100

    feat: add totalproducts store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,5 @@
-import { writable, type Writable } from "svelte/store";
+import { writable, type Writable } from 'svelte/store';
 
-export const message: Writable<string> = writable("");
+export const message: Writable<string> = writable('');
+
+export const totalProducts: Writable<number> = writable(0);
