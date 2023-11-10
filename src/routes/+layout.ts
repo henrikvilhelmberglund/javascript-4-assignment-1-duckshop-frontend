7f6efbdd2365ff6e04518ad94b993ed4bff34c40
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-10 23:06:31 +0100

    feat: initialize totalProducts when going to a route in case of refresh + we have sessionstorage data

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,12 +4,14 @@ export const prerender = false;
 
 import { FETCH_URL } from '$lib/constants';
 import type { IProduct } from '$lib/interfaces/product';
+import { countProducts } from '$lib/utils';
 
 /** @type {import('./$types').PageLoad} */
 export async function load(): Promise<{ products: IProduct[] }> {
 	const response = await fetch(`http://${FETCH_URL}:3001/api/v1/products`);
 	const data = await response.json();
 	const products: IProduct[] = data.mockData;
+	countProducts();
 	return {
 		products,
 	};
