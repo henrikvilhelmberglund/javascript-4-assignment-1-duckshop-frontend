64f14823b59a3e37fca4254c55a0852e22c3d96e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 22:54:20 +0100

    feat: add 404.html as fallback (for publishing to github pages)

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      // fallback: "error"
-    })
+		adapter: adapter({ fallback: '404.html' }),
 	},
-	vitePlugin: { inspector: { toggleKeyCombo: 'shift' } }
+	vitePlugin: { inspector: { toggleKeyCombo: 'shift' } },
 };
 
 export default config;
