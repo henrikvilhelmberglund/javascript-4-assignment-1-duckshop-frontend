94d9cd2b7cef184e06ee1abc44e5dfe2b8705b48
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 16:13:20 +0100

    feat: enable SSR and prerendering

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,15 +1,22 @@
-// disable SSR and prerendering to make sure fetches are done only in the browser
-export const ssr = false;
-export const prerender = false;
+// it is possible to disable SSR and prerendering to make sure fetches are done only in the browser
+export const ssr = true;
+export const prerender = true;
 
+import { browser } from '$app/environment';
 import { FETCH_URL } from '$lib/constants';
 import type { IProduct } from '$lib/interfaces/product';
 import { countProducts } from '$lib/utils';
 
+interface productsResponse {
+	statusCode: number;
+	message: string;
+	mockData: IProduct[];
+}
+
 /** @type {import('./$types').PageLoad} */
-export async function load(): Promise<{ products: IProduct[] }> {
+export async function load({ fetch }) {
 	const response = await fetch(`http://${FETCH_URL}:3001/api/v1/products`);
-	const data = await response.json();
+	const data: productsResponse = await response.json();
 	const products: IProduct[] = data.mockData;
 	countProducts();
 	return {
