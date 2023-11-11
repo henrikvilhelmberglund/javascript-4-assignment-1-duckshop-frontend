aae3e21684c29ba33abe8a902dc598bb0afc1aab
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 18:04:44 +0100

    feat: use parent data instead of different API route to make site work offline

--- src/routes/products/[id]/+page.ts
+++ src/routes/products/[id]/+page.ts
@@ -2,10 +2,14 @@ import { FETCH_URL } from '$lib/constants.js';
 import type { IProduct } from '$lib/interfaces/product';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ params, fetch }): Promise<{ product: IProduct }> {
-	const response = await fetch(`http://${FETCH_URL}:3001/api/v1/product/${params.id}`);
-	const data = await response.json();
-	const product: IProduct = data.mockData;
+export async function load({ parent, params, fetch }): Promise<{ product: IProduct }> {
+  const parentData = await parent();
+  const products: IProduct[] = parentData.products;
+  
+	// const response = await fetch(`http://${FETCH_URL}:3001/api/v1/product/${params.id}`);
+	// const data = await response.json();
+	// const product: IProduct = data.mockData;
+  const product = products.find((product) => product.id === +params.id);
 	return {
 		product,
 	};
