83bfb1d610d06e2b45000d7687c24478d4927096
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 16:17:05 +0100

    test: add fetch to sveltekit load function, prettier

--- src/routes/layout.unit.test.ts
+++ src/routes/layout.unit.test.ts
@@ -6,25 +6,22 @@ describe('Load function', () => {
 	it('returns a product array that has a length of at least 11', async () => {
 		// ...
 		// We must call `load` ourselves because Vitest does not do it for the moment, due to page.server not being able to be called outside of SvelteKit context (https://github.com/sveltejs/kit/issues/1485#issuecomment-920923252)
-		const data = await load();
+		const data = await load({ fetch });
 		const products: IProduct[] = data.products;
 		expect(products.length).toBeGreaterThanOrEqual(11);
 	});
 
 	describe('Product array', async () => {
-		const data = await load();
+		const data = await load({ fetch });
 		const products: IProduct[] = data.products;
 
-		it.each(products)(
-			`$name has id, name, price, image_link, amount_in_stock and description`,
-			async (product: IProduct) => {
-				expect(product.id).toBeDefined();
-				expect(product.name).toBeDefined();
-				expect(product.price).toBeDefined();
-				expect(product.description).toBeDefined();
-				expect(product.amount_in_stock).toBeDefined();
-				expect(product.image_link).toBeDefined();
-			}
-		);
+		it.each(products)(`$name has id, name, price, image_link, amount_in_stock and description`, async (product: IProduct) => {
+			expect(product.id).toBeDefined();
+			expect(product.name).toBeDefined();
+			expect(product.price).toBeDefined();
+			expect(product.description).toBeDefined();
+			expect(product.amount_in_stock).toBeDefined();
+			expect(product.image_link).toBeDefined();
+		});
 	});
 });
