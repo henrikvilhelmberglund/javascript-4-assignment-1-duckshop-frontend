ccb85906615f51ca6dcfc18f675aafd75582e54c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 23:01:27 +0100

    feat: update +error.svelte for rendering errors

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
 </script>
 
-<!-- TODO make this work -->
-<h1>{$page.status}: {$page.error.message}</h1>
+<h1 class="text-4xl">{$page.status} ({$page.error.message})</h1>
+{#if $page.error.extraInfo}
+	<h2>{$page.error.extraInfo}</h2>
+{/if}
+<img src={`https://httpducks.com/${$page.status}.jpg`} alt="" />
