cce4fb67bf5cf7bc3b6234c6de86e2b2b8b224ec
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 22:54:57 +0100

    style: prettier

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,8 +2,7 @@
 export async function handle({ event, resolve }) {
 	// UnoCSS
 	const response = await resolve(event, {
-		transformPageChunk: ({ html }) =>
-			html.replace('%unocss-svelte-scoped.global%', 'unocss_svelte_scoped_global_styles')
+		transformPageChunk: ({ html }) => html.replace('%unocss-svelte-scoped.global%', 'unocss_svelte_scoped_global_styles'),
 	});
 	// UnoCSS end
 	return response;
