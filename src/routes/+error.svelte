6fc1607d421ed02ba668290016898cc7e9ba5e64
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 23:59:54 +0100

    feat: add disclaimer

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,8 +2,10 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1 class="text-4xl">{$page.status} ({$page.error.message})</h1>
-{#if $page.error.extraInfo}
+<!-- only works when server rendered for some reason -->
+
+<h1 class="text-4xl">{$page?.status} ({$page?.error?.message})</h1>
+{#if $page?.error?.extraInfo}
 	<h2>{$page.error.extraInfo}</h2>
 {/if}
 <img src={`https://httpducks.com/${$page.status}.jpg`} alt="" />
