6cd237dfb4d2c7629feacacc2651fe41a72a7a93
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 16:10:27 +0100

    feat: add browser check to countProducts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,25 +1,30 @@
 import { get } from 'svelte/store';
 import { totalProducts } from './stores';
+import { browser } from '$app/environment';
 
-export function countProducts(): { [key: string]: number } {
-	const productsInCart = sessionStorage.getItem('cart');
-	// this fixes productCount[product] because TS knows it's a key of type string and a value of type number
-	let productCount: { [key: string]: number } = {};
-	if (productsInCart) {
-		const products = productsInCart.split(',');
-		totalProducts.set(products.length - 1);
+export function countProducts(): { [key: string]: number } | {} {
+	if (browser) {
+		const productsInCart = sessionStorage.getItem('cart');
+		// this fixes productCount[product] because TS knows it's a key of type string and a value of type number
+		let productCount: { [key: string]: number } = {};
+		if (productsInCart) {
+			const products = productsInCart.split(',');
+			totalProducts.set(products.length - 1);
 
-		// Reset the productCount object
+			// Reset the productCount object
 
-		// Count the occurrences of each product
-		products.forEach((product) => {
-			// skips last ,
-			if (product !== '') {
-				productCount[product] = (productCount[product] || 0) + 1;
-			}
-		});
-	}
-	return productCount;
+			// Count the occurrences of each product
+			products.forEach((product) => {
+				// skips last ,
+				if (product !== '') {
+					productCount[product] = (productCount[product] || 0) + 1;
+				}
+			});
+		}
+		return productCount;
+  }
+  // no sessionStorage on server
+  return {}
 }
 
 export function countOccurrences(mainString: string, substring: string): number {
