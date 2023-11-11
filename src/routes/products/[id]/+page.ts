bd51505a5c5b656fb93358bcc311852387504999
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-11 16:19:50 +0100

    feat: add sveltekit fetch in sveltekit load function

--- src/routes/products/[id]/+page.ts
+++ src/routes/products/[id]/+page.ts
@@ -2,7 +2,7 @@ import { FETCH_URL } from '$lib/constants.js';
 import type { IProduct } from '$lib/interfaces/product';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ params }): Promise<{ product: IProduct }> {
+export async function load({ params, fetch }): Promise<{ product: IProduct }> {
 	const response = await fetch(`http://${FETCH_URL}:3001/api/v1/product/${params.id}`);
 	const data = await response.json();
 	const product: IProduct = data.mockData;
