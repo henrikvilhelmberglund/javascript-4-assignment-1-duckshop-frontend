d2975628ebe6d639a7b707addd1febd334c772ce
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-12 21:59:46 +0100

    fix: use number in .env since bools don't work (oops)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 import { PUBLIC_HENRIK_PC } from '$env/static/public';
 
 // need to use a network IP when testing on mobile!
-export const FETCH_URL = PUBLIC_HENRIK_PC ? '192.168.1.64' : 'localhost';
+export const FETCH_URL = +PUBLIC_HENRIK_PC === 0 ? 'localhost' : '192.168.1.64';
