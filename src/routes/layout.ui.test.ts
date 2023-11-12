0f84ab31e03dcd2ef174fdfaf4c133a2f388c001
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-12 16:58:41 +0100

    fix: cleanup

--- src/routes/layout.ui.test.ts
+++ src/routes/layout.ui.test.ts
@@ -1,13 +1,12 @@
 import { render } from '@testing-library/svelte';
 import Layout from './+layout.svelte';
-import Page from './+page.svelte';
 import { load } from './+layout';
+import type { LayoutData } from './$types';
 
 describe('Layout route (UI test)', async () => {
-	const data = await load({ fetch });
-
 	const { container } = render(Layout, {
-		// not using this but for reference - needs export let data;
+    // not using props but for reference - needs export let data;
+    // (see unit test for actual scary code)
 		// props: {
 		// 	data,
 		// },
@@ -16,6 +15,6 @@ describe('Layout route (UI test)', async () => {
 	it('has a h1 "DuckShop"', async () => {
 		const h1 = container.querySelector('h1');
 		expect(h1).not.toBeNull();
-		expect(h1!.textContent).toBe('DuckShop'); // Replace 'Expected Text' with the text you want to check for
+		expect(h1!.textContent).toBe('DuckShop');
 	});
 });
